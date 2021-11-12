9d6c6244b51742af18d1a3bebeeb356984dd8679
OLEG <schastliviy.sluchay@ukr.net>
2021-11-12 12:28:37 +0200

    0.9

--- src/servises.js
+++ src/servises.js
@@ -4,8 +4,6 @@ const data = {
 };
 
 function sendUrl(value, page) {
-  // const baseUrl = "https://pixabay.com/api/";
-  // const apiKey = "23539275-fb90155ac37cf87d4395ca2a5";
   return `${data.baseUrl}?q=${value}&page=${page}&key=${data.apiKey}&image_type=photo&orientation=horizontal&per_page=12`;
 }
 
